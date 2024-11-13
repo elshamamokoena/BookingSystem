9d775c9083c03d2c5b7c21a16626ff24f5472c84
Elshama Mokoena <elshamamokoena@gmail.com>
2024-11-13 08:30:45 +0200

    move auth functionality into separate Identity CL

--- BookingSystem.Api/obj/Debug/net8.0/BookingSystem.Api.AssemblyInfo.cs
+++ BookingSystem.Api/obj/Debug/net8.0/BookingSystem.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a531f7e48aa86fd22f6edcb5c50beb6474d3c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f42f018fc954bee8063f9234769380230632e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BookingSystem.Identity/BookingSystemIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookingSystem.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSystem.Identity
+{
+    public class BookingSystemIdentityDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public BookingSystemIdentityDbContext(DbContextOptions<BookingSystemIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
